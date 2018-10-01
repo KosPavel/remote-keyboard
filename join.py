350c53b2a0f4d4f7a82e7498808b432e8f1721f2
kospavel <kos.pav007@yandex.ru>
2018-10-01 10:05:00 +0300

    IP input has been added

--- join.py
+++ join.py
@@ -14,12 +14,11 @@ def on_release(key):
     conn.send(bytes(data, 'UTF-8'))
 
 ''' establishing connection '''
-# conn = socket.socket()
-#
-# # HOST = input("Enter IP of remote PC")
-# HOST = socket.gethostbyname(socket.gethostname())
-# PORT = 50005
-# conn.connect((HOST, PORT))
+conn = socket.socket()
+
+HOST = input("Enter IP of remote PC")
+PORT = 50005
+conn.connect((HOST, PORT))
 
 with Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
